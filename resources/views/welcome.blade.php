7f5cef19af26a579827d17c6b0954b1c445b82e1
Darren Henderson <darrenh@ccsj.edu>
2021-11-07 15:33:33 -0600

    First commit.

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>inair practice</title>
+    <script src="https://code.highcharts.com/highcharts.js"></script>
+</head>
+<body>
+    <h1>Hello INAIR!!!</h1>
+
+    <h2>Stacked Column chart</h2>
+    <div id="container">
+
+    </div>
+
+
+    <h2>Line chart</h2>
+    <div id="container2">
+
+    </div>
+    <script src="js/line-chart.js"></script>
+    <script src="js/stacked-column-chart.js"></script>    
+</body>
+</html>
