77c9f15aaf983ff99dcc5f7b8ee59e61a3368462
Darren Henderson <darrenh@ccsj.edu>
2021-11-07 16:00:33 -0600

    Added cdn to html page for chart image export.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>inair practice</title>
     <script src="https://code.highcharts.com/highcharts.js"></script>
+    <script src="https://code.highcharts.com/modules/exporting.js"></script>
 </head>
 <body>
     <h1>Hello INAIR!!!</h1>
